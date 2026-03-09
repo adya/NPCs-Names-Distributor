ae5aba940b20f78917fad7d9ac8c7aaf9d0d06d7
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2026-03-09 17:08:05 +0200

    Fixed SE build.

--- src/main.cpp
+++ src/main.cpp
@@ -62,7 +62,7 @@ extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Query(const SKSE::QueryInterface* a
 	}
 
 	const auto ver = a_skse->RuntimeVersion();
-	if (ver < SKSE::RUNTIME_1_5_39) {
+	if (ver < SKSE::RUNTIME_SSE_LATEST) {
 		logger::critical(FMT_STRING("Unsupported runtime version {}"), ver.string());
 		return false;
 	}
